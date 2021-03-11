f108dd5d7e84d823afab6781c1a736d0c4c2523a
Valerii2390-dev <valezh14@yahoo.com>
2021-03-11 15:21:08 +0400

    added header

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    // burger menu javascript
+    $('.header__menu-burger').on('click',
+        function() {
+            $('.header__menu-list').slideToggle();
+        });
+    // burger menu javascript end
+});
